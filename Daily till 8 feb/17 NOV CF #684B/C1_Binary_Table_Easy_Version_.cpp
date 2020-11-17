dbc6878ccec3a46a8da7c9c779d38073e35bd320
gauravengine <52628750+gauravengine@users.noreply.github.com>
2020-11-17 22:18:47 +0530

    17 nov

---
+++ Daily till 8 feb/17 NOV CF #684B/C1_Binary_Table_Easy_Version_.cpp
@@ -0,0 +1,33 @@
+// Created by Yadav_Gaurav
+#include <bits/stdc++.h>
+
+#define db1(x) cout<<#x<<"="<<x<<'\n'
+#define db2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<'\n'
+#define db3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<'\n'
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define repA(i,a,n) for(int i=a;i<=(n);++i)
+#define repD(i,a,n) for(int i=a;i>=(n);--i)
+#define mod 1000000007
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+	
+        int arr[n+1][m+1];
+        for(int i=1;i<=n;i++){
+        	string s; cin>>s;
+        	for(int j=1;j<=m;j++){
+        		arr[i][j]= s[j-1];
+        	}
+        } 	
+    	
+
+
+
+        cout<<'\n';
+    }
+    return 0;
+}
