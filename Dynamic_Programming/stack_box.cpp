f5db97fb569c58f10c57cdf1bfa135fe59fe7b3d
gauravengine <52628750+gauravengine@users.noreply.github.com>
2021-02-17 11:20:07 +0530

    regular update

--- Dynamic_Programming/stack_box.cpp
+++ Dynamic_Programming/stack_box.cpp
@@ -30,17 +30,17 @@ using namespace std;
 using ll = long long;
 int32_t main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    //#ifndef ONLINE_JUDGE
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    //#endif  
-    int t;
-    cin>>t;
-    while(t--){
-        
-    }
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	//#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//#endif  
+	int t;
+	cin>>t;
+	while(t--){
+		
+	}
+	return 0;
 }
