d32a4f095654d36404552431165d3ef37310562c
gaurav <gengine232@gmail.com>
2021-06-10 21:26:06 +0530

    regular

--- a_contest/b.cpp
+++ a_contest/b.cpp
@@ -31,28 +31,28 @@ using namespace std;
 using ll = long long;
 vi init(string s)
 {
-	istringstream sin(s);
-	int n;
-	vi arr;
-	while (sin >> n)arr.push_back(n);
-	return arr;
+  istringstream sin(s);
+  int n;
+  vi arr;
+  while(sin>>n)arr.push_back(n);
+  return arr;
 }
 // int dx[]={-1,1,0,0}; int dy[]={0,0,1,-1};
 // int dx[]={2,2,-2,-2,1,1,-1,-1}; int dy[]={1,-1,1,-1,2,-2,2,-2};
 
 /*------------------------------UNORDERED MAP HASH --------------------------------------------*/
-//To make unordered_map unhackable
+//To make unordered_map unhackable 
 // use it as unordered_map<int,int,custom_hash> mapp;
 /*
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
+        // http://xorshift.di.unimi.it/splitmix64.c 
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-
+ 
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
@@ -60,56 +60,44 @@ struct custom_hash {
 };
 */
 /*------------------------------END--------------------------------------------*/
-inline int powMod(int a, int b) { int x = 1; while (b > 0) { if (b & 1) x = (x * a) % MOD; a = (a * a) % MOD; b >>= 1; } return x; }
+inline int powMod(int a, int b) { int x = 1; while (b > 0) { if (b&1) x = (x*a) % MOD; a = (a*a) % MOD; b >>= 1; } return x; }
 inline int multiply(int x, int y) { return ((x % MOD) * (y % MOD)) % MOD; }
-inline int divide(int x, int y) { return ((x % MOD) * powMod(y % MOD, MOD - 2)) % MOD; }
-inline int ceil(int a, int b) { return (a + b - 1) / b; }
+inline int divide(int x, int y) { return ((x % MOD) * powMod(y % MOD, MOD-2)) % MOD; }
+inline int ceil(int a, int b) { return (a+b-1)/b; }
 int gcd (int a, int b) { while (b) { a %= b; swap(a, b); } return a; }
 int lcm (int a, int b) { return a / gcd(a, b) * b; }
 int inverseMod(int a, int m) { a = a % m; for (ll x = 1; x < m; x++) if ((a * x) % m == 1) return x; return -1; }
 
-template<int D, typename T> struct vec : public vector < vec < D - 1, T >> { static_assert(D >= 1, "Vector dimension must be greater than zero!");  template<typename... Args> vec(int n = 0, Args... args) : vector < vec < D - 1, T >> (n, vec < D - 1, T > (args...)) { } }; template<typename T> struct vec<1, T> : public vector<T> { vec(int n = 0, T val = T()) : vector<T>(n, val) { }};
+template<int D, typename T> struct vec : public vector<vec<D - 1, T>> { static_assert(D >= 1, "Vector dimension must be greater than zero!");  template<typename... Args> vec(int n = 0, Args... args) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(args...)) { } }; template<typename T> struct vec<1, T> : public vector<T> { vec(int n = 0, T val = T()) : vector<T>(n, val) { }};
 
 
-void solve() {
-	int n;
-	cin >> n;
-	string s;
-	cin >> s;
-	string ans = "";
-	set<string> ss;
-
-	for (int len = 1; len <= 3; len++) {
-		if(len==1)for (int i = 0; i < 26; i++) ss.insert( string(1, (char)(i + 'a')) );
-		else if(len==2)for (int i = 0; i < 26; i++)for (int j = 0; j < 26; j++)ss.insert(string(1, (char)(i + 'a')) + string(1, (char)(j + 'a')) );
-		else for (int i = 0; i < 26; i++)for (int j = 0; j < 26; j++)for (int k = 0; k < 26; k++)ss.insert(string(1, (char)(i + 'a')) + string(1, (char)(j + 'a')) + string(1, (char)(k + 'a')) );
-		for (int i = 0; i + len <= n; i++) {
-			string temp = s.substr(i, len);
-			if (ss.count(temp))  ss.erase(temp);
-		}
-		if(!ss.empty()){
-			ans=*(ss.begin());
-			break;
-		}
-
-	}
-
-	
-	cout << ans << '\n';
+void solve(){
+    int n;
+    cin>>n;
+    vec<1,int> a(n,0);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            int x; cin>>x;
+            if(i==j) continue;
+            a[i]|=x;
+            a[j]|=x;
+        }
+    }
+    for(int i=0;i<n;i++) cout<<a[i]<<" ";
 }
 
 int32_t main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	//#ifndef ONLINE_JUDGE
-	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-	//#endif
-	int t = 1;
-	cin >> t;
-	while (t--) solve();
-
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //#ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //#endif  
+    int t=1;
+    //cin>>t;
+    while(t--) solve();
+    
+    return 0;
 }
--- usaco_rand()/company_queries.cpp
+++ usaco_rand()/company_queries.cpp
@@ -31,28 +31,28 @@ using namespace std;
 using ll = long long;
 vi init(string s)
 {
-    istringstream sin(s);
-    int n;
-    vi arr;
-    while (sin >> n)arr.push_back(n);
-    return arr;
+  istringstream sin(s);
+  int n;
+  vi arr;
+  while(sin>>n)arr.push_back(n);
+  return arr;
 }
 // int dx[]={-1,1,0,0}; int dy[]={0,0,1,-1};
 // int dx[]={2,2,-2,-2,1,1,-1,-1}; int dy[]={1,-1,1,-1,2,-2,2,-2};
 
 /*------------------------------UNORDERED MAP HASH --------------------------------------------*/
-//To make unordered_map unhackable
+//To make unordered_map unhackable 
 // use it as unordered_map<int,int,custom_hash> mapp;
 /*
 struct custom_hash {
     static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
+        // http://xorshift.di.unimi.it/splitmix64.c 
         x += 0x9e3779b97f4a7c15;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
     }
-
+ 
     size_t operator()(uint64_t x) const {
         static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
         return splitmix64(x + FIXED_RANDOM);
@@ -60,62 +60,62 @@ struct custom_hash {
 };
 */
 /*------------------------------END--------------------------------------------*/
-inline int powMod(int a, int b) { int x = 1; while (b > 0) { if (b & 1) x = (x * a) % MOD; a = (a * a) % MOD; b >>= 1; } return x; }
+inline int powMod(int a, int b) { int x = 1; while (b > 0) { if (b&1) x = (x*a) % MOD; a = (a*a) % MOD; b >>= 1; } return x; }
 inline int multiply(int x, int y) { return ((x % MOD) * (y % MOD)) % MOD; }
-inline int divide(int x, int y) { return ((x % MOD) * powMod(y % MOD, MOD - 2)) % MOD; }
-inline int ceil(int a, int b) { return (a + b - 1) / b; }
+inline int divide(int x, int y) { return ((x % MOD) * powMod(y % MOD, MOD-2)) % MOD; }
+inline int ceil(int a, int b) { return (a+b-1)/b; }
 int gcd (int a, int b) { while (b) { a %= b; swap(a, b); } return a; }
 int lcm (int a, int b) { return a / gcd(a, b) * b; }
 int inverseMod(int a, int m) { a = a % m; for (ll x = 1; x < m; x++) if ((a * x) % m == 1) return x; return -1; }
 
-template<int D, typename T> struct vec : public vector < vec < D - 1, T >> { static_assert(D >= 1, "Vector dimension must be greater than zero!");  template<typename... Args> vec(int n = 0, Args... args) : vector < vec < D - 1, T >> (n, vec < D - 1, T > (args...)) { } }; template<typename T> struct vec<1, T> : public vector<T> { vec(int n = 0, T val = T()) : vector<T>(n, val) { }};
+template<int D, typename T> struct vec : public vector<vec<D - 1, T>> { static_assert(D >= 1, "Vector dimension must be greater than zero!");  template<typename... Args> vec(int n = 0, Args... args) : vector<vec<D - 1, T>>(n, vec<D - 1, T>(args...)) { } }; template<typename T> struct vec<1, T> : public vector<T> { vec(int n = 0, T val = T()) : vector<T>(n, val) { }};
+//vec<1,int> parent(200005);
+int n,q;
+// 200005
+vec<2,int> up(200005,20,-1),adj(200005);
+void dfs(int u,int p){
+	up[u][0]=p;
+	for(int j=1;j<20;j++){
+		if(up[u][j-1]!=-1)
+			up[u][j]=up[up[u][j-1]][j-1];
+		else 
+			up[u][j]=-1; // j-1 is -1 no jump above it
+	}
+	for(auto v:adj[u]){
+		if(p==v) continue;
+		dfs(v,u);
+	}
 
-
-void solve() {
-    int n, k;
-    cin >> n >> k;
-    unordered_map<int, vi> hash;
-    vec<1, int> arr(n);
-    for (int i = 0; i < n; i++) cin >> arr[i];
-    string s;
-    cin >> s;
-    int temp = 0;
-    for (auto c : s) {
-        hash[c - 'a'].pb(temp++);
-    }
-    int dmg = 0;
-    for (int i = 0; i < 26; i++) {
-        auto v = hash[i];
-        if (v.size() == 0) continue;
-        pqb pq;
-        pq.push(arr[v[0]]);
-        for (int j = 1; j < v.size(); j++) {
-            if (v[j] - v[j - 1] > 1) {
-
-                //choose k elements
-                //add to answer
-                int count = k;
-                while (count-- && !pq.empty()) {
-                    auto val = pq.top();
-                    pq.pop();
-                    dmg += val;
-                }
-                pq = pqb();
-            }
-            pq.push(arr[v[j]]);
-
-        }
-        //empty the full pq now
-        int count = k;
-        while (count-- && !pq.empty()) {
-            auto val = pq.top();
-            pq.pop();
-            dmg += val;
-        }
-    }
-
-    cout << dmg;
-
+}
+void solve(){
+	cin>>n>>q;
+	//parent[1]=1;
+	for(int i=2;i<=n;i++){
+		
+		//
+		int u;
+		cin>>u;
+		adj[u].pb(i);
+	}
+	// 1 is my root here
+	up[1][0]=-1;
+	//looks like i have to call on children also no row jump from 1 to 2
+	dfs(1,-1);
+	while(q--){
+		int x,k;
+		cin>>x>>k;
+		int p1=x;
+		for(int j=0;j<20;j++){
+			if(k&(1<<j)){
+				p1=up[p1][j];
+				//TODO
+				if(p1==-1){
+					break; //lol
+				}
+			}
+		}
+		cout<<p1<<'\n';
+	}
 }
 
 int32_t main()
@@ -126,10 +126,10 @@ int32_t main()
     //#ifndef ONLINE_JUDGE
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
-    //#endif
-    int t = 1;
+    //#endif  
+    int t=1;
     //cin>>t;
-    while (t--) solve();
-
+    while(t--) solve();
+    
     return 0;
 }
